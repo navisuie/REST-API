12f18b52f4fc6cefaeb510caabe996c58bdb9e94
navisuie <navisuie@gmail.com>
2024-05-27 13:42:23 -0500

    added isOwner middleware
    checks to make sure the user is the owner of account before deleting

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 
 import { getAllUsers, deleteUser } from '../controllers/users'
-import { isAuth } from '../middlewares'
+import { isAuth, isOwner } from '../middlewares'
 
 
 
 export default (router: express.Router) => {
     router.get('/users', isAuth, getAllUsers)
-    router.delete('/users/:id', deleteUser)
+    router.delete('/users/:id', isOwner, deleteUser)
 }
