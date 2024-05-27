cb494f097f0d8bf3bbedbca9b8e4522ba10d7589
navisuie <navisuie@gmail.com>
2024-05-27 11:29:06 -0500

    fix importing issue

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import { createUser, getUserByEmail } from 'db/users';
-import { authentication, random } from 'helpers';
+import { createUser, getUserByEmail } from '../db/users';
+import { authentication, random } from '../helpers';
 
 export const register = async (req: express.Request, res: express.Response) => {
     try {
