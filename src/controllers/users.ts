f0e89dc1a184361626535f301a06fa92ad4f2591
navisuie <navisuie@gmail.com>
2024-05-27 13:36:11 -0500

    added delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-import { getUsers } from '../db/users'
+import { getUsers, deleteUserById } from '../db/users'
+
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -7,6 +8,19 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
         return res.status(200).json(user)
     }
 
+    catch (err) {
+        console.log(err)
+        return res.sendStatus(400)
+    }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params
+        const deletedUser = await deleteUserById(id)
+
+        return res.json(deletedUser)
+    }
     catch (err) {
         console.log(err)
         return res.sendStatus(400)
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 
-import { getAllUsers } from '../controllers/users'
+import { getAllUsers, deleteUser } from '../controllers/users'
+import { isAuth } from '../middlewares'
+
+
 
 export default (router: express.Router) => {
-    router.get('/users', getAllUsers)
+    router.get('/users', isAuth, getAllUsers)
+    router.delete('/users/:id', deleteUser)
 }
