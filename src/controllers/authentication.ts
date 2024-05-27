23f73ff9fb233700fedd141630be9c894d5a2d34
navisuie <navisuie@gmail.com>
2024-05-27 11:36:44 -0500

    Update authentication.ts

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -5,12 +5,13 @@ import { authentication, random } from '../helpers';
 export const register = async (req: express.Request, res: express.Response) => {
     try {
         const { email, password, username } = req.body;
-        if (!email || password || username) {
+        if (!email || !password || !username) {
             return res.sendStatus(400);
         }
         const existingUser = await getUserByEmail(email);
         if (existingUser) {
             return res.sendStatus(400);
+
         }
 
         const salt = random();
@@ -26,6 +27,7 @@ export const register = async (req: express.Request, res: express.Response) => {
     } catch (err) {
         console.log(err);
         return res.sendStatus(400);
+
     }
 
 }
